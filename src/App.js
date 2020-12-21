121aa58e2b5ec61666546f0926280632ed1da5b8
oni193 <keykeykey193@gmail.com>
2020-12-21 11:33:33 +0900

    ➕ Add Service App.js에 Service 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Service from './Service';
 import Map from './Map';
 
 function App() {
   return (
     <div>
     <Navigation/>
+    <Service/>
     <Map/>
     </div>
   );
