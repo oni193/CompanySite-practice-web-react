161ecc90f6a264fca7d45f639daa144b3ae3d5db
oni193 <keykeykey193@gmail.com>
2020-12-21 10:55:36 +0900

    ➕ Add Map     App.js에 Map 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Map from './Map';
 
 function App() {
   return (
     <div>
     <Navigation/>
+    <Map/>
     </div>
   );
 }
