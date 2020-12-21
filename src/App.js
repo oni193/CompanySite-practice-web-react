0794e8fb9d07ebeb2988f4eb946431e5601f8bc4
oni193 <keykeykey193@gmail.com>
2020-12-21 16:02:25 +0900

    ➕ Add Recruit App.js에 Recruit import

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Navigation from './Navigation';
 import Service from './Service';
 import Partner from './Partner';
-import Map from './Map';
+import Recruit from './Recruit';
+import Map from './Map'; 
+import Modal from './components/Recruit/Modal';
 
 function App() {
   return (
     <div>
+    {/* <Modal/> */}
     <Navigation/>
     <Service/>
     <Partner/>
+    <Recruit/>
     <Map/>
     </div>
   );
