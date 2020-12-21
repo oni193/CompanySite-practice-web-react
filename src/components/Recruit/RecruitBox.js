3e45c75bc489ae452b15947a483bb53a3d9fd782
oni193 <keykeykey193@gmail.com>
2020-12-21 16:11:42 +0900

    ✨ Add Service Components ( box .. ) 그러나.. 내 맘처럼 되지 않는다.. ㅎㅏ.. 어떻게 해야 하느뇨..

---
+++ src/components/Recruit/RecruitBox.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Body from '../textSize/Body';
+
+const RecruitBox = () => {
+
+    const boxStyle = {
+        height: '400px',
+        backgroundColor: '#ffc9c9'
+    }
+
+    const bodyStyle = {
+        width: '50px',
+        height: '50px',
+        border: '1px solid black',
+    }; 
+
+    const data = {
+        body : [ '기획/시스템팀', '신사업개발팀', 'IT운영팀', '관리팀/온라인팀', '지원하기']
+    };
+    
+    const backColor = {
+        color: [
+            '#c92a2a', '#faa2c1', '#e9ecef', '#f8f0fc', '#e5dbff', '#99e9f2'
+        ]
+    }
+
+    // const array = [
+    //     {body: '디지털 사업팀', color: '#c92a2a'},
+    //     {body: '신사업개발팀', color: '#faa2c1'}
+    // ];
+
+    return(
+        <div style={boxStyle}>
+            <div style={bodyStyle} >
+                <Body text={data.body[0]}  />
+            </div>
+            {/* for문으로 하면 될거 같은딬 */}
+        </div>
+    );
+};
+
+export default RecruitBox;
