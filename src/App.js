657c749082d6a1ae990b3f3f586f64581df63969
oni193 <keykeykey193@gmail.com>
2020-12-22 13:18:42 +0900

    💄 Changes look good Modal 잠시 빼고 조금 더 보기 편하게 바꿈

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Service from './Service';
 import Partner from './Partner';
 import Recruit from './Recruit';
 import Map from './Map'; 
-import Modal from './components/Recruit/Modal';
+// import Modal from './components/Recruit/Modal';
 
 function App() {
   return (
-    <div>
-    {/* <Modal/> */}
-    <Navigation/>
-    <Service/>
-    <Partner/>
-    <Recruit/>
-    <Map/>
-    </div>
+
+      <div>
+        {/* <Modal/> */}
+        <Navigation/>
+        <Service/>
+        <Partner/>
+        <Recruit/>
+        <Map/>
+      </div>
+
+    
   );
 }
 
