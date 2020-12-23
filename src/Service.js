aec9808c1f1391b6aa475c6e878c985c62b65e87
oni193 <keykeykey193@gmail.com>
2020-12-23 14:41:35 +0900

    💄 Look Better

--- src/Service.js
+++ src/Service.js
@@ -6,9 +6,9 @@ import Description from './components/textSize/Description';
 // import Internal from './components/service/Internal';
 
 import img0 from './assets/1.png';
-// import img1 from './assets/2.jpg';
-// import img2 from './assets/3.png';
-// import img3 from './assets/4.jpg';
+import img1 from './assets/2.jpg';
+import img2 from './assets/3.png';
+import img3 from './assets/4.jpg';
 
 const Service = () => {
     
@@ -41,8 +41,22 @@ const Service = () => {
 
 
                 <div className="serviceRight" style={serviceStyle}>
+                    <div>
                         <img src={img0} style={imgStyle} alt="img"/>       
                         <Description text={des[0]}/>
+                    </div>
+                    <div>
+                        <img src={img1} style={imgStyle} alt="img"/>       
+                        <Description text={des[1]}/>
+                    </div>
+                    <div>
+                        <img src={img2} style={imgStyle} alt="img"/>       
+                        <Description text={des[2]}/>
+                    </div>
+                    <div>
+                        <img src={img3} style={imgStyle} alt="img"/>       
+                        <Description text={des[3]}/>
+                    </div>
                 </div>
             </div> 
     );
