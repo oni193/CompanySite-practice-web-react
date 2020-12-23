7622c59e4d7dd16d1ad880ce5627541f2d9f8a81
oni193 <keykeykey193@gmail.com>
2020-12-23 09:37:12 +0900

    ➕ Add BrowserRouter index 페이지 browserRouter 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,4 +12,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 reportWebVitals();
