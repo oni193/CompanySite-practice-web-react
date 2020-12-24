291caf887aa916fe7979bba5b02dcba167d3d2b9
oni193 <keykeykey193@gmail.com>
2020-12-24 09:06:07 +0900

    💄 Change looks good

--- src/components/Recruit/Modal.js
+++ src/components/Recruit/Modal.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const ModalStyle={
+    position: 'absolute',
+    top: '500px',
+    width: '100%',
+    height: '300px',
+    backgroundColor: 'rgba(52, 52, 52, 0.8)',
+    color: 'white',
+};
+
 const Modal = ({ isShowing, hide, text }) => isShowing ? ReactDOM.createPortal(
-  <React.Fragment>
-    <div className="modal-overlay"/>
+  <>
+  
     <div className="modal-wrapper" >
       <div className="modal">
-        <div className="modal-header">
-          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+        {/* Modal 창 내용 */}
+        <div style={ModalStyle}>
+            <button type="button" data-dismiss="modal" aria-label="Close" onClick={hide}>
             <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div> {text} </div>
+            </button> 
+            {text} 
+            </div>
       </div>
     </div>
-  </React.Fragment>, document.body
+  </>, document.body
 ) : null;
 
 export default Modal;
