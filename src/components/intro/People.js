7bd8ff81a40105853de5869180a385c512eb3442
oni193 <keykeykey193@gmail.com>
2020-12-30 13:10:11 +0900

    🚧 Add 3 person

--- src/components/intro/People.js
+++ src/components/intro/People.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-// import Icon from '../../assets/peopleIcon.png';
+import PeopleIcon from '../../assets/peopleIcon.png';
 
 
-const People = () => {
+const People = ({...props}) => {
 
 
     const FinelabPeople = styled.div`
@@ -12,18 +12,56 @@ const People = () => {
         border : 1px solid black;
         background-color : rgb(232,232,232);
     `;
+    const Icons = styled.div`
+        display : flex;
+        flex-direction : row;
+    `;
     const Icon = styled.img`
-        width : 50px;
+        width : 100px;
         height: auto;
+        margin-right : -40px;
         src : Icon;
     `;
+    Icon.defaultProps = {
+        src : PeopleIcon,
+    };
+
 
     return(
         <FinelabPeople>
-            <div>파인랩 사람들 &#62; </div>
-            <div>
-                <Icon/>
+            <Icons>
+                <div
+                data-aos="fade-up"
+                data-aos-offset="200"
+                data-aos-delay="150"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+            >
+                <Icon {...props}/>
             </div>
+            <div
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="300"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+        >
+                <Icon {...props}/>
+            </div>
+            <div
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="400"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+        >
+                <Icon {...props}/>
+            </div> 
+            </Icons>
+            <div>파인랩 사람들 &#62; </div>
         </FinelabPeople>
     );
 };
