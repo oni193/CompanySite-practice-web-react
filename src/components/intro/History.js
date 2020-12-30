ef17f25d34e5795a2a01ccfda4d6f0c4a00213b1
oni193 <keykeykey193@gmail.com>
2020-12-30 11:25:32 +0900

    🎉 Add Intro's Layout

--- src/components/intro/History.js
+++ src/components/intro/History.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 
 const History = () => {
 
+
+    const FinelabPeople = styled.div`
+        width : auto;
+        height : 270px;
+        border : 1px solid black;
+    `;
+
     return(
-        <div></div>
+        <FinelabPeople>
+            '오른쪽 history'
+        </FinelabPeople>
     );
 };
 
--- src/components/intro/People.js
+++ src/components/intro/People.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+// import Icon from '../../assets/peopleIcon.png';
 
 
 const People = () => {
 
+
+    const FinelabPeople = styled.div`
+        width : auto;
+        height : 270px;
+        border : 1px solid black;
+        background-color : rgb(232,232,232);
+    `;
+    const Icon = styled.img`
+        width : 50px;
+        height: auto;
+        src : Icon;
+    `;
+
     return(
-        <div></div>
+        <FinelabPeople>
+            <div>파인랩 사람들 &#62; </div>
+            <div>
+                <Icon/>
+            </div>
+        </FinelabPeople>
     );
 };
 
