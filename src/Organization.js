8c812422f71c196066c6458194f3d414fd518178
oni193 <keykeykey193@gmail.com>
2020-12-31 10:29:29 +0900

    🚧 Add Footer to Organization

--- src/Organization.js
+++ src/Organization.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import Image from './assets/organization.jpg'
+import Footer from './Footer.js';
 
 const Organization = () => {
     const Box = styled.div`
         width: auto;
-        height: 600px;
+        height: 700px;
         padding-left : 25%;
         padding-right : 10%;
         margin-top: 200px;
     `;
 
     return(
-        <Box>
-            <img src ={Image}/>
-        </Box>
+        <div>
+            <Box>
+                <img src ={Image}/>
+            </Box>
+            <Footer/>
+        </div>
     );
 };
 
